2a2a7624d2a979b4fb4353785fbc4af4698d5d55
Safuan04 <elmai.safuan@gmail.com>
2024-01-25 16:22:10 +0100

    4. Retrieving lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -35,7 +35,7 @@ def call_history(method: Callable) -> Callable:
     return function_wrapper
 
 
-def replay(method: Callable) -> None:
+def replay(method: Callable):
     """This function display the history of
         calls of a particular function."""
     r = redis.Redis()
